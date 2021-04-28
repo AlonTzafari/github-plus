dceebf65def07baa9c13bf898c27cb3012f8b1ea
AlonTzafari <77238176+AlonTzafari@users.noreply.github.com>
2021-04-28 15:30:34 +0300

    add option element to popup

--- background.js
+++ background.js
@@ -1,5 +1,7 @@
+let ENABLED = true;
+
 chrome.tabs.onUpdated.addListener( (tabId, changeInfo) => {
-    if (changeInfo.status == 'complete') {
+    if (changeInfo.status === 'complete' && ENABLED) {
         chrome.tabs.get(tabId, tab_info => {
             if ( /^https:\/\/github/.test(tab_info.url) ) {
                 chrome.tabs.executeScript(null, {file: "./foreground.js"}, () => console.log("script activated"));
---
+++ popup.js
@@ -0,0 +1,9 @@
+document.addEventListener('DOMContentLoaded', () => {
+    const toggleBtn = document.querySelector('#enable');
+    toggleBtn.addEventListener('click', event => {
+    });
+});
+
+
+
+
