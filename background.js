ab6d94a76ee3f5b210a1168e9adf02b1fdb2637a
AlonTzafari <77238176+AlonTzafari@users.noreply.github.com>
2021-04-28 14:52:46 +0300

    code cleanup

--- background.js
+++ background.js
@@ -1,24 +1,4 @@
-console.log("from background");
-
-// chrome.tabs.onActivated.addListener(tab => {
-//     console.log(tab);
-//     chrome.tabs.get(tab.tabId, tab_info => {
-//         console.log(tab_info);
-//         if ( /^https:\/\/github/.test(tab_info.url) ) {
-//             chrome.tabs.executeScript(null, {file: "./foreground.js"}, () => console.log("script injected"));
-//         }
-//     });
-// });
-
-// chrome.runtime.onMessage.addListener( (request, sender, sendResponse) => {
-//     console.log(request);
-//     if (request.type === "opened rich diff") {
-//         typingHistory.push(request.content);
-//         sendResponse({ok: true});
-//     }
-// });
-
-chrome.tabs.onUpdated.addListener( (tabId, changeInfo, tab) => {
+chrome.tabs.onUpdated.addListener( (tabId, changeInfo) => {
     if (changeInfo.status == 'complete') {
         chrome.tabs.get(tabId, tab_info => {
             if ( /^https:\/\/github/.test(tab_info.url) ) {
--- foreground.js
+++ foreground.js
@@ -1,10 +1,7 @@
-console.log('from foreground');
- 
 (() => {
     const richDiffButtons = document.querySelectorAll('button[aria-label="Display the rich diff"]');
     if (richDiffButtons.length >= 1) {
         const btnArray = Array.from(richDiffButtons);
-        console.log(btnArray);
         btnArray.forEach(button => {
             button.click();
         });
