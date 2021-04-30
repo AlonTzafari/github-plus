9869f9c2a04782ae42fd1be0b7a1444e488cb301
AlonTzafari <77238176+AlonTzafari@users.noreply.github.com>
2021-04-30 14:54:14 +0300

    read and set enabled option from storage

--- src/eventPage.ts
+++ src/eventPage.ts
@@ -1,11 +1,25 @@
+let ENABLED : boolean;
+
+function loadOptions () : void {
+    chrome.storage.sync.get(['enabled'], ({enabled}) => {
+        console.log('enabled: ', enabled);
+        if (typeof enabled === 'undefined') {
+            chrome.storage.sync.set({'enabled': true});
+            ENABLED = true;
+            return;
+        } 
+        ENABLED = enabled;
+    })
+}
+loadOptions();
+
 // Listen to messages sent from other parts of the extension.
 chrome.runtime.onMessage.addListener((request, sender, sendResponse) => {
-    // onMessage must return "true" if response is async.
-    let isResponseAsync = false;
-
     if (request.popupMounted) {
         console.log('eventPage notified that Popup.tsx has mounted.');
     }
 
-    return isResponseAsync;
+    if (request.loadOptions) loadOptions();    
 });
+
+0
