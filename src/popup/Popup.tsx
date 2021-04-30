0f1b29daa287dab16e4b24dd35d60dd56d0253c4
AlonTzafari <77238176+AlonTzafari@users.noreply.github.com>
2021-04-30 16:47:08 +0300

    enable button

--- src/popup/Popup.tsx
+++ src/popup/Popup.tsx
@@ -1,11 +1,22 @@
 import React, { useEffect } from "react";
 import "./Popup.scss";
+import Option from './Option';
 
 export default function Popup() {
+  
   useEffect(() => {
     // Example of how to send a message to eventPage.ts.
     chrome.runtime.sendMessage({ popupMounted: true });
   }, []);
 
-  return <div className="popupContainer">Hello, world!</div>;
+  const toggleEnable = () => {
+
+  }
+
+  return (
+    <div className="popup">
+      <h2>options</h2>
+      <Option title="Enabled" optionName="enabled"/>
+    </div>
+  );
 }
