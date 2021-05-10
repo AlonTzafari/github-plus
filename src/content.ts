6655620497c878efe51033383928a25d5501840a
AlonTzafari <77238176+AlonTzafari@users.noreply.github.com>
2021-05-10 16:33:11 +0300

    code cleanup

--- src/content.ts
+++ src/content.ts
@@ -19,7 +19,6 @@
     })
 
     function openRichDiffView() : void {
-        
         const richDiffButtons = document.querySelectorAll('button[aria-label="Display the rich diff"]');
         if (richDiffButtons.length >= 1) {
             const btnArray = Array.from(richDiffButtons);
